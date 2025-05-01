cee242f4b5099f90d164b3bd8ea4c842739d1361
Fullbust505 <linusnedellec@gmail.com>
2025-05-01 23:56:21 +0200

    added firebase link

--- app/src/main/java/com/fullbust/orphy/MainActivity.kt
+++ app/src/main/java/com/fullbust/orphy/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Linus t'es vachement beau mais t es un $name!",
         modifier = modifier
     )
 }
