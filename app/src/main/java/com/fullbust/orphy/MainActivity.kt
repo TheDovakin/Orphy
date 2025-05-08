af1d47172e1082bb31c51839d02ca189128bcd0f
Fullbust505 <linusnedellec@gmail.com>
2025-05-08 20:48:43 +0200

    anonymous auth
    made on the main activity since it is essential at the start of the app (and I don't know how to put it on another page and link them)

--- app/src/main/java/com/fullbust/orphy/MainActivity.kt
+++ app/src/main/java/com/fullbust/orphy/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.fullbust.orphy
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,10 +14,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.fullbust.orphy.ui.theme.OrphyTheme
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
+
+const val TAG = "Main Activity"
 
 class MainActivity : ComponentActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
         enableEdgeToEdge()
         setContent {
             OrphyTheme {
@@ -26,6 +37,40 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
+        }
+    }
+    public override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        updateUI(currentUser);
+
+        auth.signInAnonymously()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInAnonymously:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInAnonymously:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    fun updateUI(user: FirebaseUser?) {
+        if (user != null){
+            val userInfo = "User ID: ${user.uid}\nEmail: ${user.email ?: "No email"}"
+            Log.d(TAG, userInfo)
+        } else{
+            Log.d(TAG, "gros flop")
         }
     }
 }
